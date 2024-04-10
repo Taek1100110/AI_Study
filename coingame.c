175b1f30fd3bc31ea24aaa1f53fffab877c78415
Taek1100110 <144909508+Taek1100110@users.noreply.github.com>
2024-04-10 22:37:40 +0900

    Update coingame.c

--- coingame.c
+++ coingame.c
@@ -6,6 +6,9 @@ for roll in range(sample):
 	else:
 		frequency1 += 1 */
 
+/* 파이썬과 달리 C언어는 간단한 동전 던지기 프로그램을 만들 때에도 3개의 헤더파일 선언이 필요하며, 난수 생성을 위해 srand함수르 추가적으로 사용해야 한다.
+   또한 rand함수를 사용하기 위해 모듈러스 연산이 추가적으로 필요하다. 그리고 매우 간단한 파이썬의 for문과 달리 C언어에서는 초기식, 조건식, 변환식이 필요하다. */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
